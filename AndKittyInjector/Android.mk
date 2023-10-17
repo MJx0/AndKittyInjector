7bb98f781d5921dbf92f551b119dce1483e00fa1
MJ <32413318+MJx0@users.noreply.github.com>
2023-10-17 15:19:15 -0700

    added xdl

--- AndKittyInjector/Android.mk
+++ AndKittyInjector/Android.mk
@@ -3,6 +3,9 @@ LOCAL_PATH := $(call my-dir)
 KITTYMEMORY_PATH = $(LOCAL_PATH)/../KittyMemoryEx/KittyMemoryEx
 KITTYMEMORY_SRC  = $(wildcard $(KITTYMEMORY_PATH)/*.cpp)
 
+XDL_PATH = $(LOCAL_PATH)/src/xdl
+XDL_SRC  = $(wildcard $(XDL_PATH)/*.c)
+
 ## Example exec
 include $(CLEAR_VARS)
 
@@ -12,9 +15,9 @@ LOCAL_MODULE := AndKittyInjector
 # use logcat logging to get outputs in realtime
 LOCAL_CPPFLAGS += -std=c++17 -DkNO_KEYSTONE #-DkUSE_LOGCAT -DkITTYMEMORY_DEBUG
 
-LOCAL_C_INCLUDES += $(KITTYMEMORY_PATH)
+LOCAL_C_INCLUDES += $(KITTYMEMORY_PATH) $(XDL_PATH)
 
 PROJ_SRC = $(wildcard $(LOCAL_PATH)/src/*.cpp) $(wildcard $(LOCAL_PATH)/src/Injector/*.cpp)
-LOCAL_SRC_FILES := $(PROJ_SRC) $(KITTYMEMORY_SRC)
+LOCAL_SRC_FILES := $(PROJ_SRC) $(KITTYMEMORY_SRC) $(XDL_SRC)
 
 include $(BUILD_EXECUTABLE)
