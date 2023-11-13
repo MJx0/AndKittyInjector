1e5eae5cf327a6d19b63d24bd15d94a76da0f8c8
MJ <32413318+MJx0@users.noreply.github.com>
2023-11-13 01:50:33 -0800

    improve cmake build script

--- AndKittyInjector/cmake-build.bat
+++ AndKittyInjector/cmake-build.bat
@@ -1,23 +1,35 @@
 @ECHO OFF
 
+:: Path to ndk
 SET "NDK=%NDK_HOME%"
-SET "ABI=arm64-v8a"
-SET "HOST=windows-x86_64"
+
+:: Path to cmake
 SET "CMAKE=cmake"
+
+:: Path to cmake
 SET "MAKE=make"
 
-SET "BUILD_PATH=cmake_build/%ABI%"
+SET BUILD_PATH=cmake_build
 
-CMAKE -S. -B%BUILD_PATH% -G "Unix Makefiles" ^
--DCMAKE_EXPORT_COMPILE_COMMANDS=TRUE ^
--DCMAKE_BUILD_TYPE=Release ^
--DCMAKE_TOOLCHAIN_FILE=%NDK%/build/cmake/android.toolchain.cmake ^
--DCMAKE_C_COMPILER=%NDK_HOME%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang.exe ^
--DCMAKE_CXX_COMPILER=%NDK_HOME%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang++.exe ^
--DANDROID_NDK=%NDK% ^
--DANDROID_ABI=%ABI% ^
--DANDROID_NATIVE_API_LEVEL=21
+:: Targets
+SET "ABIs=arm64-v8a armeabi-v7a x86 x86_64"
 
-MAKE -C%BUILD_PATH% -j16
+for %%x in (%ABIs%) do (
+    ECHO ==========================
+    ECHO = Building %%x
+    ECHO ==========================
+
+    CMAKE -S. -B%BUILD_PATH%/%%x -G "Unix Makefiles" ^
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=TRUE ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_TOOLCHAIN_FILE=%NDK%/build/cmake/android.toolchain.cmake ^
+    -DCMAKE_C_COMPILER=%NDK_HOME%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang.exe ^
+    -DCMAKE_CXX_COMPILER=%NDK_HOME%\toolchains\llvm\prebuilt\windows-x86_64\bin\clang++.exe ^
+    -DANDROID_NDK=%NDK% ^
+    -DANDROID_ABI=%%x ^
+    -DANDROID_NATIVE_API_LEVEL=21
+
+    MAKE -C%BUILD_PATH%/%%x -j16
+)
 
 PAUSE
